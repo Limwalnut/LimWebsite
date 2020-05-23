87db1b6acbca56813e4c58b5e9ddb0f89de4017c
Hanli Zhang <zhanghanli007@gmail.com>
2020-05-23 13:51:37 +0800

    database setting

--- LimWebsite/app/models.py
+++ LimWebsite/app/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Picture(models.Model):
+    PictureId = models.CharField('Id', primary_key=True, max_length=15)
+    PictureName = models.CharField('Name', max_length=50)
